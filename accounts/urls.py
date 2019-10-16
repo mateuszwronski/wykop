8e32f26c3a60da4a0f679091421ed1a7c0d4a810
unknown <wronsky7@gmail.com>
2019-10-16 20:33:26 +0200

    login & registration app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.views import LoginView
+from accounts.views import RegistrationView
 from django.urls import path
 
 app_name = 'accounts'
 
 urlpatterns = [
     path("login/", LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path("register/", RegistrationView.as_view(), name='register')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+
+class RegistrationView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'accounts/register.html'
+    success_url = '/posts/'
